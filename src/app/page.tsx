59f08173e52b88c0547b5781d433e1ed795260fd
oboil <parkmisonme777@gmail.com>
2025-12-29 16:03:43 +0900

    feat: 카테고리 선택 구현 & fix: components를 use client로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,15 +19,33 @@ interface Article {
 export default function Home() {
   const [loading, setLoading] = useState(false);
 
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [selectedCategory, setSelectedCateory] = useState<number | null>(null);
+
   const [articles, setArticles] = useState<Article[]>([]);
   const [hasMore, setHasMore] = useState(true);
   const [offset, setOffset] = useState(0);
   const LIMIT = 6;
 
+  // 카테고리 목록 불러오기
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch("YOUR_DJANGO_CATEGORY_API_URL");
+      const data = await response.json();
+      setCategories(data);
+    } catch (error) {
+      console.error("카테고리 불러오기 실패: ", error);
+    }
+  };
+
   // 기사 목록 불러오기
-  const fetchArticles = async (currentOffset: number) => {
+  const fetchArticles = async (
+    currentOffset: number,
+    categortId: number | null = null
+  ) => {
     setLoading(true);
     try {
+      const categoryParam = categortId ? `&category_id=${categortId}` : "";
       const response = await fetch(
         `YOUR_DJANGO_API_URL?limit=${LIMIT}&offset=${currentOffset}`
       );
@@ -39,7 +57,7 @@ export default function Home() {
         setArticles((prev) => [...prev, ...data]);
       }
 
-      setHasMore(data.legnth === LIMIT);
+      setHasMore(data.length === LIMIT);
     } catch (error) {
       console.error("포스트 불러오기 실패: ", error);
     } finally {
@@ -48,8 +66,15 @@ export default function Home() {
   };
 
   useEffect(() => {
+    fetchCategories();
     fetchArticles(0);
   }, []);
+
+  const handleCategoryClick = (categoryId: number | null) => {
+    setSelectedCateory(categoryId);
+    setOffset(0); // offset 초기화 (해당 카테고리 게시글로 다시 로딩)
+    fetchArticles(0, categoryId);
+  };
 
   const handleLoadMore = () => {
     const nextOffset = offset + LIMIT;
@@ -63,7 +88,27 @@ export default function Home() {
         <HomeHeader />
         <section className="carousel_wrap"></section>
         <section className="articles_list_wrap">
-          <div className="category_div"></div>
+          <div className="category_div">
+            <div
+              className={`categort_badge ${
+                selectedCategory === null ? "active" : ""
+              }`}
+              onClick={() => handleCategoryClick(null)}
+            >
+              전체보기
+            </div>
+            {categories.map((category) => (
+              <div
+                key={category.id}
+                className={`categort_badge ${
+                  selectedCategory === category.id ? "active" : ""
+                }`}
+                onClick={() => handleCategoryClick(category.id)}
+              >
+                {category.name}
+              </div>
+            ))}
+          </div>
           <div className="articles_list_div">
             {articles.map((article) => (
               <div key={article.id} className="article_item">
